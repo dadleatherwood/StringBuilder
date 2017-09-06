1860a5f5033db8e9b21b08f3b2e0ae5ee26a2b6e
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-06 08:11:59 -0500

    Completed Notes

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Text;
 
-namespace StringBuilder
+namespace StringBuilder101
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new StringBuilder();
+
+            //apend method
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+
+            //Replace
+            builder.Replace('-', '+');
+
+            //Remove
+            builder.Remove(0, 10);
+
+            //Insert
+            builder.Insert(0, new string('-', 10));
+
+            //You can chain methods
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Hello")
+                .AppendLine()
+                .Append('-', 10);
+
+            Console.WriteLine(builder);
         }
     }
 }
